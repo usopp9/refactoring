2eef56b004e0c2acd07b1f69273700d3dbac5e65
usopp <usopp9@naver.com>
2018-03-19 12:24:35 +0900

    리팩토링 완료

--- src/main/java/refactoring/Main.java
+++ src/main/java/refactoring/Main.java
@@ -6,9 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		Robot robot = new Robot("Andrew");
-		robot.order(0);//walk
-		robot.order(1);//stop
-		robot.order(2);//jump
+		robot.order(Robot.COMMAND_WALK);
+		robot.order(Robot.COMMAND_STOP);
+		robot.order(Robot.COMMAND_JUMP);
 	}
-
 }
--- src/main/java/refactoring/chap01/Robot.java
+++ src/main/java/refactoring/chap01/Robot.java
@@ -1,6 +1,9 @@
 package refactoring.chap01;
 
 public class Robot {
+	public static final int COMMAND_JUMP = 2;
+	public static final int COMMAND_STOP = 1;
+	public static final int COMMAND_WALK = 0;
 	private final String _name;
 
 	public Robot(String _name) {
@@ -8,11 +11,11 @@ public class Robot {
 	}
 	
 	public void order(int command) {
-		if(command==0) {
+		if(command==COMMAND_WALK) {
 			System.out.printf("%s %s%n",_name, "walks.");
-		}else if(command==1) {
+		}else if(command==COMMAND_STOP) {
 			System.out.printf("%s %s%n",_name, "stops.");
-		}else if(command==2) {
+		}else if(command==COMMAND_JUMP) {
 			System.out.printf("%s %s%n",_name, "jumps.");
 		}else {
 			System.out.printf("%s %s%n","Command error. command=",command);

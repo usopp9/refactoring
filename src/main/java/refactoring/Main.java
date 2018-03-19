31c6bcae656b259214d211b1ecb5795be8fe2dde
usopp <usopp9@naver.com>
2018-03-19 11:41:03 +0900

    Main add build.gradle 수정

---
+++ src/main/java/refactoring/Main.java
@@ -0,0 +1,14 @@
+package refactoring;
+
+import refactoring.chap01.Robot;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Robot robot = new Robot("Andrew");
+		robot.order(0);//walk
+		robot.order(1);//stop
+		robot.order(2);//jump
+	}
+
+}

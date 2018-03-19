fd27d206592cadc279eff5341cadd7acbffc4cb3
usopp <usopp9@naver.com>
2018-03-19 11:30:38 +0900

    Robot.java

--- src/main/java/refactoring/chap01/Robot.java
+++ src/main/java/refactoring/chap01/Robot.java
@@ -1,5 +1,21 @@
 package refactoring.chap01;
 
 public class Robot {
+	private final String _name;
 
+	public Robot(String _name) {
+		this._name = _name;
+	}
+	
+	public void order(int command) {
+		if(command==0) {
+			System.out.printf("%s %s%n",_name, "walks.");
+		}else if(command==1) {
+			System.out.printf("%s %s%n",_name, "stops.");
+		}else if(command==2) {
+			System.out.printf("%s %s%n",_name, "jumps.");
+		}else {
+			System.out.printf("%s %s%n","Command error. command=",command);
+		}
+	}
 }

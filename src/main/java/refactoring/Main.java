db9af581d5739e87d35b78f2673c5a9056146e87
usopp <usopp9@naver.com>
2018-03-20 11:47:39 +0900

    chap03.sort

--- src/main/java/refactoring/Main.java
+++ src/main/java/refactoring/Main.java
@@ -3,16 +3,39 @@ package refactoring;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Random;
 
 import refactoring.chap01.Robot;
 import refactoring.chap01.Simple.SimpleDatabase;
 import refactoring.chap02.FindInt;
+import refactoring.chap03.SortSample;
 
 public class Main {
-
+	private static Random random = new Random(1234);
 	public static void main(String[] args) {
 		//testChap01();
 		//testChap02();
+		//testChap02_1();
+		
+		execute(10);
+		execute(10);
+		execute(10);
+		execute(10);
+		execute(10);
+		
+	}
+	private static void execute(int length) {
+		int[] data = new int[length];
+		for(int i =0;i<data.length;i++) {
+			data[i]= random.nextInt(data.length);
+		}
+		SortSample sorter = new SortSample(data);
+		System.out.println("BEFORE: "+sorter);
+		sorter.sort();
+		System.out.println("AFTER: "+sorter);
+		System.out.println();
+	}
+	private static void testChap02_1() {
 		try {
 
 			String fileUrl = Thread.currentThread().getContextClassLoader().getResource("dbfile.txt").getPath();

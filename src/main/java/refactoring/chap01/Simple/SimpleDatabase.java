ca29cbb8deaf3dec0ce4d186602c208d4a912d1c
usopp <usopp9@naver.com>
2018-03-20 11:22:23 +0900

    simpledata

--- src/main/java/refactoring/chap01/Simple/SimpleDatabase.java
+++ src/main/java/refactoring/chap01/Simple/SimpleDatabase.java
@@ -8,34 +8,31 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class SimpleDatabase {
-	private Map<String,String> _map= new HashMap<>();
-	public SimpleDatabase(Reader r) throws IOException{
+	private Map<String, String> _map = new HashMap<>();
+
+	public SimpleDatabase(Reader r) throws IOException {
 		BufferedReader reader = new BufferedReader(r);
-	
-		while(true){
-			
-			String line = reader.readLine();
-			if(line==null) {
-				break;
-			}else {
-				int equalIndex = line.indexOf("=");
-				if(equalIndex >0) {
-					String key =  line.substring(0, equalIndex);
-					String value = line.substring(equalIndex + 1, line.length());
-					_map.put(key, value);
-				}
+		String line;
+		while ((line = reader.readLine()) != null) {
+			int equalIndex = line.indexOf("=");
+			if (equalIndex > 0) {
+				String key = line.substring(0, equalIndex);
+				String value = line.substring(equalIndex + 1, line.length());
+				_map.put(key, value);
 			}
 		}
 	}
-		public void putValue(String key, String value) {
-			_map.put(key, value);
-		}
-		public String getValue(String key) {
-			return _map.get(key);
-		}
-		public Iterator<String> iterator(){
-			return _map.keySet().iterator();
-		}
-	
+
+	public void putValue(String key, String value) {
+		_map.put(key, value);
+	}
+
+	public String getValue(String key) {
+		return _map.get(key);
+	}
+
+	public Iterator<String> iterator() {
+		return _map.keySet().iterator();
+	}
 
 }

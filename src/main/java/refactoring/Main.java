89d38811a6768abb45711f7d97f52e0d58706321
usopp <usopp9@naver.com>
2018-03-20 10:29:01 +0900

    flag

--- src/main/java/refactoring/Main.java
+++ src/main/java/refactoring/Main.java
@@ -1,10 +1,21 @@
 package refactoring;
 
 import refactoring.chap01.Robot;
+import refactoring.chap02.FindInt;
 
 public class Main {
 
 	public static void main(String[] args) {
+		//testChap01();
+		int[] data = {1,9,0,2,8,5,6,3,4,7};
+		if(FindInt.find(data, 5)) {
+			System.out.println("Found!");
+		}else {
+			System.out.println("Not found...");
+		}
+	}
+
+	private static void testChap01() {
 		Robot robot = new Robot("Andrew");
 		robot.order(0);//walk
 		robot.order(1);//stop

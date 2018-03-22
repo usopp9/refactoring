a5068a394ce4f80750295e57071997f504079e9d
usopp <usopp9@naver.com>
2018-03-22 10:12:29 +0900

    chap06.book

---
+++ src/main/java/refactoring/chap06/Book.java
@@ -0,0 +1,56 @@
+package refactoring.chap06;
+
+public class Book {
+	private String title;
+	private String isbn;
+	private String price;
+	private String authorName;
+	private String authorMail;
+	public Book(String title, String isbn, String price, String authorName, String authorMail) {
+		super();
+		this.title = title;
+		this.isbn = isbn;
+		this.price = price;
+		this.authorName = authorName;
+		this.authorMail = authorMail;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	public String getAuthorMail() {
+		return authorMail;
+	}
+	public void setAuthorMail(String authorMail) {
+		this.authorMail = authorMail;
+	}
+	
+	public String toXml() {
+		String autor = tag("author", tag("name","authorName")+tag("mail","authorMail"));
+		String book = tag("book", tag("title", title)+tag("isbn",isbn)+tag("price",price)+autor);
+		return book;
+	}
+	private String tag(String element, String content) {
+		return String.format("<%1$s>%2$s</%1$s>%n", element,content);
+	}
+}	

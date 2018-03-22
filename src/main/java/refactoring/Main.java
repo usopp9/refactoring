334cec29492816e9a0da0333e45f26b8538e9ff2
usopp <usopp9@naver.com>
2018-03-22 11:04:22 +0900

    player.클래스 추출전

--- src/main/java/refactoring/Main.java
+++ src/main/java/refactoring/Main.java
@@ -13,6 +13,7 @@ import refactoring.chap04.Label;
 import refactoring.chap04.Person;
 import refactoring.chap05.Banner;
 import refactoring.chap06.Book;
+import refactoring.chap06.extract_superclass.Player;
 
 public class Main {
 	private static Random random = new Random(1234);
@@ -22,8 +23,23 @@ public class Main {
 		//testChap02_1();		
 		//testChap03();
 		//testChap04();
-		//testChap05();
+		//testChap05();		
+		//testChap06();
 		
+		Player musicPlayer = new Player();
+		musicPlayer.setCurrentMedia(true);
+		Player videoPlayer = new Player();
+		videoPlayer.setCurrentMedia(false);
+		
+		play(musicPlayer);
+		play(videoPlayer);		
+	}
+	private static void play(Player player) {
+		player.play();
+		player.loop();
+		player.stop();
+	}
+	private static void testChap06() {
 		Book math = new Book("프로그래머 수학","ISBN4797329734","20000원","유키 히로시","hyuki@hyuki.com");
 		System.out.println("math: ");
 		System.out.println(math.toXml());

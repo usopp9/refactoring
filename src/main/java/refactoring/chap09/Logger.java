b99cdf262231515cddbf56707ed2dd3f704cf260
usopp <usopp9@naver.com>
2018-03-26 14:36:56 +0900

    chapter09.after

--- src/main/java/refactoring/chap09/Logger.java
+++ src/main/java/refactoring/chap09/Logger.java
@@ -4,36 +4,45 @@ public class Logger {
 	public static final int STATE_STOPPED = 0;
 	public static final int STATE_LOGGING = 1;
 	private int state;
+	
 	public Logger() {
-		state= STATE_STOPPED;
+		setState(STATE_STOPPED);
 	}
 	
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+
 	public void start() {
-		switch(state) {
+		switch(getState()) {
 		case STATE_STOPPED:
 			System.out.println("** START LOGGING **");
-			state= STATE_LOGGING;
+			setState(STATE_LOGGING);
 			break;
 		case STATE_LOGGING:
 			break;
 		default:
-			System.out.println("Invalid state : "+state);
+			System.out.println("Invalid state : "+getState());
 		}
 	}
 	public void stop() {
-		switch (state) {
+		switch (getState()) {
 			case STATE_STOPPED:
 				break;
 			case STATE_LOGGING:
 				System.out.println("** STOP LOGGING **");
-				state = STATE_STOPPED;
+				setState(STATE_STOPPED);
 				break;
 			default:
-				System.out.println("Invalid state : "+state);
+				System.out.println("Invalid state : "+getState());
 		}
 	}
 	public void log(String info) {
-		switch (state) {
+		switch (getState()) {
 		case STATE_STOPPED:
 			System.out.println("Ignoring : "+info);
 			break;
@@ -41,7 +50,7 @@ public class Logger {
 			System.out.println("Logging : "+ info);
 			break;
 		default:
-			System.out.println("Invalid state : "+state);
+			System.out.println("Invalid state : "+getState());
 		}
 	}
 }

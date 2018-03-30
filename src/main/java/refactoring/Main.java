0faa53a5c7d9d09cb3f278638fbbdeaa4a86a990
usopp <usopp9@naver.com>
2018-03-30 14:08:55 +0900

    chap13.master

--- src/main/java/refactoring/Main.java
+++ src/main/java/refactoring/Main.java
@@ -22,23 +22,39 @@ import refactoring.chap09.Logger;
 import refactoring.chap10.Robot;
 import refactoring.chap11.Shape;
 import refactoring.chap12.IntegerDisplay;
+import refactoring.chap13.Dice;
 
 public class Main {
 	private static Random random = new Random(1234);
+
 	public static void main(String[] args) {
-		//testChap01();
-		//testChap02();
-		//testChap02_1();		
-		//testChap03();
-		//testChap04();
-		//testChap05();		
-		//testChap06();
-		//testChap07();
-		//testChap08();
-		//testChap08_1();
-		//testChap09();
-		//testChap10();
-		//testChap11();
+		// testChap01();
+		// testChap02();
+		// testChap02_1();
+		// testChap03();
+		// testChap04();
+		// testChap05();
+		// testChap06();
+		// testChap07();
+		// testChap08();
+		// testChap08_1();
+		// testChap09();
+		// testChap10();
+		// testChap11();
+		// testChap12();
+		List<Dice> dices = Arrays.asList(new Dice(), new Dice(456L), new Dice());
+		dices.get(2).setSeed(456L);
+
+		for (Dice d : dices) {
+			for (int i = 0; i < 10; i++) {
+				System.out.printf("%d%s ", d.nextInt(), i < 9 ? "," : "");
+			}
+			System.out.println();
+		}
+
+	}
+
+	private static void testChap12() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -50,97 +66,102 @@ public class Main {
 			}
 		});
 	}
+
 	private static void testChap11() {
-		List<Shape> shapes = Arrays.asList(
-				new Shape(Shape.TYPECODE_LINE, 0, 0, 100, 200),
+		List<Shape> shapes = Arrays.asList(new Shape(Shape.TYPECODE_LINE, 0, 0, 100, 200),
 				new Shape(Shape.TYPECODE_RECTANGLE, 10, 20, 30, 40),
-				new Shape(Shape.TYPECODE_OVAL, 100, 200, 300, 400)
-				);
-		
-		for(Shape s :shapes) {
+				new Shape(Shape.TYPECODE_OVAL, 100, 200, 300, 400));
+
+		for (Shape s : shapes) {
 			s.draw();
 		}
 	}
+
 	private static void testChap10() {
 		Robot robot = new Robot("Andrew");
 		System.out.println(robot.toString());
-		
+
 		robot.execute("forward right forward");
 		System.out.println(robot.toString());
-		
+
 		robot.execute("left backward left forward");
 		System.out.println(robot.toString());
-		
+
 		robot.execute("right forward forward farvard");
 		System.out.println(robot.toString());
 	}
+
 	private static void testChap09() {
 		Logger logger = new Logger();
 		logger.log("infomation #1");
-		
+
 		logger.start();
 		logger.log("infomation #2");
 		logger.start();
 		logger.log("infomation #3");
-		
+
 		logger.stop();
 		logger.log("infomation #4");
 		logger.stop();
 		logger.log("infomation #5");
 	}
+
 	private static void testChap08_1() {
 		List<refactoring.chap08.Shape> shapes = Arrays.asList(
-					new refactoring.chap08.Shape(refactoring.chap08.Shape.TYPECODE_LINE, 0, 0, 100,200),
-					new refactoring.chap08.Shape(refactoring.chap08.Shape.TYPECODE_RECTANGLE, 10, 20, 30,40),
-					new refactoring.chap08.Shape(refactoring.chap08.Shape.TYPECODE_OVAL, 100, 200, 300,400)
-				);
-		for(refactoring.chap08.Shape s :shapes) {
+				new refactoring.chap08.Shape(refactoring.chap08.Shape.TYPECODE_LINE, 0, 0, 100, 200),
+				new refactoring.chap08.Shape(refactoring.chap08.Shape.TYPECODE_RECTANGLE, 10, 20, 30, 40),
+				new refactoring.chap08.Shape(refactoring.chap08.Shape.TYPECODE_OVAL, 100, 200, 300, 400));
+		for (refactoring.chap08.Shape s : shapes) {
 			s.draw();
 		}
 	}
+
 	private static void testChap08() {
 		Item book = new Item(ItemType.Book, "세계 역사", 4800);
 		Item dvd = new Item(ItemType.DVD, "뉴욕의 꿈 특별판", 3000);
 		Item soft = new Item(ItemType.SOFTWARD, "튜링 머신", 3200);
-		System.out.printf("%5s = %s%n","book",book);
-		System.out.printf("%5s = %s%n","dvd",dvd);
-		System.out.printf("%5s = %s%n","soft",soft);
+		System.out.printf("%5s = %s%n", "book", book);
+		System.out.printf("%5s = %s%n", "dvd", dvd);
+		System.out.printf("%5s = %s%n", "soft", soft);
 	}
+
 	private static void testChap07() {
 		Player musicPlayer = new Player();
 		musicPlayer.setCurrentMedia(true);
 		Player videoPlayer = new Player();
 		videoPlayer.setCurrentMedia(false);
-		
+
 		play(musicPlayer);
 		play(videoPlayer);
 	}
+
 	private static void play(Player player) {
 		player.play();
 		player.loop();
 		player.stop();
 	}
+
 	private static void testChap06() {
-		Book math = new Book("프로그래머 수학","ISBN4797329734","20000원","유키 히로시","hyuki@hyuki.com");
+		Book math = new Book("프로그래머 수학", "ISBN4797329734", "20000원", "유키 히로시", "hyuki@hyuki.com");
 		System.out.println("math: ");
 		System.out.println(math.toXml());
 	}
+
 	private static void testChap05() {
 		Banner hello = new Banner("Hello, World!");
 		hello.print(3);
 	}
+
 	private static void testChap04() {
-		Person[] people = {
-				new Person(new Label("Alice"), new Label("alice@example.com")),
-				new Person(new Label("Bobby"), new Label("bobby@example.com")),
-				new Person(new Label("Chris")),
-		};
-		for(Person p : people) {
+		Person[] people = { new Person(new Label("Alice"), new Label("alice@example.com")),
+				new Person(new Label("Bobby"), new Label("bobby@example.com")), new Person(new Label("Chris")), };
+		for (Person p : people) {
 			System.out.println(p);
 			p.display();
 			System.out.println();
 		}
 	}
+
 	private static void testChap03() {
 		execute(10);
 		execute(10);
@@ -148,38 +169,40 @@ public class Main {
 		execute(10);
 		execute(10);
 	}
+
 	private static void execute(int length) {
 		int[] data = new int[length];
-		for(int i =0;i<data.length;i++) {
-			data[i]= random.nextInt(data.length);
+		for (int i = 0; i < data.length; i++) {
+			data[i] = random.nextInt(data.length);
 		}
 		SortSample sorter = new SortSample(data);
-		System.out.println("BEFORE: "+sorter);
+		System.out.println("BEFORE: " + sorter);
 		sorter.sort();
-		System.out.println("AFTER: "+sorter);
+		System.out.println("AFTER: " + sorter);
 		System.out.println();
 	}
+
 	private static void testChap02_1() {
 		try {
 			String fileUrl = Thread.currentThread().getContextClassLoader().getResource("dbfile.txt").getPath();
 			SimpleDatabase db = new SimpleDatabase(new FileReader(fileUrl));
 			Iterator<String> it = db.iterator();
-			while(it.hasNext()) {
+			while (it.hasNext()) {
 				String key = it.next();
-				System.out.println("KEY: \""+key+"\"");
-				System.out.println("Value: \""+db.getValue(key)+"\"");
+				System.out.println("KEY: \"" + key + "\"");
+				System.out.println("Value: \"" + db.getValue(key) + "\"");
 				System.out.println();
 			}
-		}catch(IOException e ) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	private static void testChap02() {
-		int[] data = {1,9,0,2,8,5,6,3,4,7};
-		if(FindInt.find(data, 5)) {
+		int[] data = { 1, 9, 0, 2, 8, 5, 6, 3, 4, 7 };
+		if (FindInt.find(data, 5)) {
 			System.out.println("Found!");
-		}else {
+		} else {
 			System.out.println("Not found...");
 		}
 	}
